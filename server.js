370d0a9ae199c88e5f11cea6c2dd91de5e8b37df
Baron Skidmore <baronhockeyplayer@gmail.com>
2022-05-08 18:54:49 -0400

    connected to jawsdb

--- server.js
+++ server.js
@@ -1,17 +1,99 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('./helper/uuid');
 
-// Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up body parsing, static, and route middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-// Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+// get the notes from db.json
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    res.status(200).json(JSON.parse(data))
+  console.info(`${req.method} request recieved to GET notes!`)
+  }
+  );
+})
+
+// delete a note from db.json
+app.delete('/api/notes/:id', (req, res) => { 
+  const filteredNotes = [];
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    console.info(`delete request called`)
+    console.log(req.params.id);
+    let noteNote = JSON.parse(data);
+    for (let i = 0; i < noteNote.length; i++) {
+     if (noteNote[i].id !== req.params.id) {
+     filteredNotes.push(noteNote[i]);
+    }}
+    console.log(filteredNotes);
+    
+    fs.writeFile('./db/db.json', JSON.stringify(filteredNotes, null, 2),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Sucessfully deleted a note!')
+        );
+    
+        res.send('body');
+  })
+  
+})
+
+// post new note to db.json
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid()
+    };
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.err(err);
+      } else {
+        console.log(data);
+        const parsedNotes = JSON.parse(data);
+       parsedNotes.push(newNote);
+
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 2),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Sucessfully wrote a new note!')
+        );
+      }
+    })
+
+    const response = {
+      status: 'Success!',
+      body: newNote
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in creating note');
+  }
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+app.listen(PORT, () =>
+  console.log(`listening at http://localhost:${PORT}`)
+);
